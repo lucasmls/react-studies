7dccaf860cd624347d3794c56156311ce9cd8a10
Lucas Mendes <lucaslsbh1@gmail.com>
2018-07-16 22:28:08 -0300

    Instalação e demonstração mais básica das PropTypes (Link de referência à documentação)

--- src/App.js
+++ src/App.js
@@ -1,34 +1,13 @@
 import React, { Component } from 'react'
-import Timer from './Timer'
+import RButton from './RButton'
 
 class App extends Component {
-  constructor () {
-    super()
-    console.log('App.js constructor')
-    this.state = {
-      showTimer: true,
-      time: 5
-    }
-  }
-
-  //WARNING! To be deprecated in React v17. Use componentDidMount instead.
-  componentWillMount() {
-    console.log('App.js will mount')
-  }
-
-  componentDidMount() {
-    console.log('App.js did mount')
-  }
-
   render() {
-    console.log('App.js render')
     return (
       <div className="container">
-        <Timer time={this.state.time} />
-        <button
-          onClick={() => this.setState({ time: this.state.time +10 })}>
-          Add +1
-        </button>
+        <RButton handleClick={() => alert('RButton')}>
+          RButton
+        </RButton>
       </div>
     );
   }
--- src/RButton.js
+++ src/RButton.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 const RButton = ({ children, handleClick }) => {
   return (
@@ -8,6 +9,13 @@ const RButton = ({ children, handleClick }) => {
         { children }!
       </button>
   )
+}
+
+// WARNING
+// Para checar todos os PropTypes existentes, checar a documentação
+// https://reactjs.org/docs/typechecking-with-proptypes.html
+RButton.propTypes = {
+  handleClick: PropTypes.func.isRequired
 }
 
 export default RButton
