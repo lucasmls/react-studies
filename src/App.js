3c0c58d62105731960bbec19def062db1384cc9f
Lucas Mendes <lucaslsbh1@gmail.com>
2018-07-18 20:04:15 -0300

    Demonstração do controlled textarea

--- src/App.js
+++ src/App.js
@@ -12,23 +12,11 @@ class App extends Component {
     return (
       <div className="container">
         <form action="">
-          <label htmlFor="">Uncontrolled</label>
-          <select>
-            <option value="1">Opção 01</option>
-            <option value="2">Opção 02</option>
-            <option value="3">Opção 03</option>
-          </select>
-
-          <br/>
-
-          <label htmlFor="">Controlled</label>
-          <select value={this.state.value} onChange={e => (
-            this.setState({ value: e.target.value })
-          )}>
-            <option value="1">Opção 01</option>
-            <option value="2">Opção 02</option>
-            <option value="3">Opção 03</option>
-          </select>
+          <textarea
+            cols="30"
+            rows="10"
+            value={this.state.value}
+            onChange={e => ( this.setState({ value: e.target.value }) )} />
         </form>
       </div>
     );
