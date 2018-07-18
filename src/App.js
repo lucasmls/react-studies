f531040bc8a425e2c735547bf6fbd73ec327fa1c
Lucas Mendes <lucaslsbh1@gmail.com>
2018-07-18 20:57:44 -0300

    Demonstração da adição de um evento de onChange no formulário

--- src/App.js
+++ src/App.js
@@ -5,18 +5,27 @@ class App extends Component {
     super()
     this.state = {
       value: '2',
+      textValue: ''
     }
   }
 
   render() {
     return (
       <div className="container">
-        <form action="">
-          <textarea
-            cols="30"
-            rows="10"
-            value={this.state.value}
-            onChange={e => ( this.setState({ value: e.target.value }) )} />
+        <form onSubmit={e => { e.preventDefault(); console.log(this.state) }}
+              onChange={e => { console.log(e.target.name); } }>
+          <textarea name="conteudo"
+                    cols="30"
+                    rows="10"
+                    value={this.state.value}
+                    onChange={e => ( this.setState({ value: e.target.value }) )} />
+            
+            <input type="text"
+                  name="input"
+                  value={this.state.textValue}
+                  onChange={e => ( this.setState({ textValue: e.target.value }) )} />
+
+            <button type="submit">Submit</button>
         </form>
       </div>
     );
