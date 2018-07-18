cc8df6edcca96afda917494318b5932fb10038eb
Lucas Mendes <lucaslsbh1@gmail.com>
2018-07-17 22:38:40 -0300

    Demonstração de controlled e uncontrolled checkbox/radio inputs

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ class App extends Component {
   constructor () {
     super()
     this.state = {
-      value: ''
+      value: '',
+      checked: false,
+      rdCheckedValue: '2'
     }
   }
 
@@ -12,10 +14,63 @@ class App extends Component {
     return (
       <div className="container">
         <form action="">
-          <input type="text" value={this.state.value} onChange={e => {
-            console.log(e)
-            this.setState({ value: e.target.value })
-          }}/>
+          <label htmlFor="controlled-input">Controlled Text Input:</label> <br/>
+          <input
+            id="controlled-input"
+            type="text"
+            value={this.state.value}
+            onChange={e => (this.setState({ value: e.target.value }) )}
+          />
+          <br/><hr/>
+          
+          <label htmlFor="uncontrolled-input">Uncontrolled Text Input:</label> <br/>
+          <input
+            type="text"
+            defaultValue="uncontrolled-input"
+          />
+          <br/><hr/>
+
+          <label htmlFor="controlled-checkbox">Controlled Checkbox</label> <br/>
+          <input
+            type="checkbox"
+            name="check"
+            id="controlled-checkbox"
+            checked={this.state.checked} onChange={e => (this.setState({
+                checked: e.target.checked })
+            )}
+          />
+          <br/><hr/>
+
+          <label htmlFor="uncontrolled-checkbox">Uncontrolled Checkbox</label> <br/>
+          <input
+            type="checkbox"
+            name="uncontrolled-checkbox"
+            id="uncontrolled-checkbox"
+            defaultChecked
+          />
+          <br/><hr/>
+
+          <label>
+            <input
+              type="radio"
+              value="1"
+              checked={this.state.rdCheckedValue === '1'}
+              onChange={ e => (this.setState({ rdCheckedValue: e.target.value })) }
+            />
+            Controlled Radio 01
+          </label> <br/>
+
+          <label>
+            <input
+              type="radio"
+              value="2"
+              checked={this.state.rdCheckedValue === '2'}
+              onChange={ e => (this.setState({ rdCheckedValue: e.target.value })) }
+            />
+            Controlled Radio 02
+          </label>
+
+
         </form>
       </div>
     );
