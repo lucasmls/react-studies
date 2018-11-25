c3fe7383e5b124fcd9adc9e04fd9a8bfa065be39
Lucas Mendes <lucaslsbh1@gmail.com>
2018-11-24 22:41:40 -0200

    [redux-todo] - Markup do form de criação dos todos

---
+++ redux-todo-list/src/containers/AddTodo.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const AddTodo = () => {
+  return (
+    <div>
+      <form action="">
+        <input type="text"/>
+        <button>Adicionar</button>
+      </form>
+    </div>
+  )
+}
+
+export default AddTodo;
