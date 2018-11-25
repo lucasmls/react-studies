ef9491a0c9bbd25965d09e947055836073eefb9c
Lucas Mendes <lucaslsbh1@gmail.com>
2018-11-24 23:08:18 -0200

    [redux-todo] - Config do redux dev tools

--- redux-todo-list/src/index.js
+++ redux-todo-list/src/index.js
@@ -7,7 +7,10 @@ import App from './containers/App'
 
 import rootReducer from './reducers'
 
-const store = createStore(rootReducer)
+const store = createStore(
+  rootReducer, /* preloadedState, */
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
   <Provider store={store}>
