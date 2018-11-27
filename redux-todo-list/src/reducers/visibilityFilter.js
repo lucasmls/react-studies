abc710dc7a74d6355171927e4ea497e7819e295d
Lucas Mendes <lucaslsbh1@gmail.com>
2018-11-26 23:38:09 -0200

    [redux-todo] - Reducer inicial de "visibilityFilter" dos todos

---
+++ redux-todo-list/src/reducers/visibilityFilter.js
@@ -0,0 +1,10 @@
+import { VisibilityFilters } from '../actions'
+
+const visibilityFilter = (state = VisibilityFilters.SHOW_ALL, action) => {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
+
+export default visibilityFilter
