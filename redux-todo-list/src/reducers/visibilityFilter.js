e22f8ef43479728f60e9c1ca88e1fda8eda4aa21
Lucas Mendes <lucaslsbh1@gmail.com>
2018-11-26 23:49:21 -0200

    [redux-todo] - Criando o "case" de SET_VISIBILITY_FILTER no reducer de visibilityFilter

--- redux-todo-list/src/reducers/visibilityFilter.js
+++ redux-todo-list/src/reducers/visibilityFilter.js
@@ -2,6 +2,8 @@ import { VisibilityFilters } from '../actions'
 
 const visibilityFilter = (state = VisibilityFilters.SHOW_ALL, action) => {
   switch (action.type) {
+    case 'SET_VISIBILITY_FILTER':
+      return action.filter
     default:
       return state
   }
