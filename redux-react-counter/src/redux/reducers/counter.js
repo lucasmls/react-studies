2a63aab5f1807bfeac50f474cb0c9ad5a07ef7a2
Lucas Mendes <lucaslsbh1@gmail.com>
2018-12-16 00:24:55 -0200

    [redux-todo] [refactor] - Retornando constantes com os nomes das actions do reducer de counter, para que  diminua o risco de escrever o nome da action errada na string

--- redux-react-counter/src/redux/reducers/counter.js
+++ redux-react-counter/src/redux/reducers/counter.js
@@ -1,15 +1,21 @@
-const counter = (state = [], action) => {
+export const ADD_COUNTER = 'ADD_COUNTER'
+export const REMOVE_COUNTER = 'REMOVE_COUNTER'
+export const INCREMENT = 'INCREMENT'
+export const DECREMENT = 'DECREMENT'
+
+export const initialState = []
+const counter = (state = initialState, action) => {
   switch (action.type) {
-    case 'ADD_COUNTER':
+    case ADD_COUNTER:
       return [ ...state, 0 ]
 
-    case 'REMOVE_COUNTER':
+    case REMOVE_COUNTER:
       return state.filter((counter, index) => index !== action.index)
 
-    case 'INCREMENT':
+    case INCREMENT:
       return state.map((counter, index) => index === action.index ? counter +1 : counter)
 
-    case 'DECREMENT':
+    case DECREMENT:
       return state.map((counter, index) => index === action.index ? counter -1 : counter)
 
     default:
--- redux-react-counter/src/redux/reducers/counter.test.js
+++ redux-react-counter/src/redux/reducers/counter.test.js
@@ -1,5 +1,5 @@
 import { expect } from 'chai'
-import counter from './counter'
+import counter, { ADD_COUNTER, REMOVE_COUNTER, INCREMENT, DECREMENT, initialState } from './counter'
 
 describe('Counter', () => {
   it('Counter should be a function', () => {
@@ -15,7 +15,7 @@ describe('Counter', () => {
 
   it('Should return the initial state if the last state is undefined', () => {
     const before = undefined
-    const after = []
+    const after = initialState
     const action = {}
     expect(counter(before, action)).to.be.deep.equal(after)
   })
@@ -23,56 +23,56 @@ describe('Counter', () => {
   it('Should add a counter', () => {
     const before = []
     const after = [0]
-    const action = { type: 'ADD_COUNTER' }
+    const action = { type: ADD_COUNTER }
     expect(counter(before, action)).to.be.deep.equal(after)
   })
 
   it('Should add another counter', () => {
     const before = [0, 10, 20]
     const after = [0, 10, 20, 0]
-    const action = { type: 'ADD_COUNTER' }
+    const action = { type: ADD_COUNTER }
     expect(counter(before, action)).to.be.deep.equal(after)
   })
 
   it('Should remove a specific counter', () => {
     const before = [1, 2, 3, 4, 5, 6]
     const after = [1, 2, 3, 5, 6]
-    const action = { type: 'REMOVE_COUNTER', index: 3 }
+    const action = { type: REMOVE_COUNTER, index: 3 }
     expect(counter(before, action)).to.be.deep.equal(after)
   })
 
   it('Should remove another specific counter', () => {
     const before = [10, 20, 30, 10]
     const after = [10, 30, 10]
-    const action = { type: 'REMOVE_COUNTER', index: 1 }
+    const action = { type: REMOVE_COUNTER, index: 1 }
     expect(counter(before, action)).to.be.deep.equal(after)
   })
 
   it('Should increment a specific counter', () => {
     const before = [1, 2, 3]
     const after = [1, 3, 3]
-    const action = { type: 'INCREMENT', index: 1 }
+    const action = { type: INCREMENT, index: 1 }
     expect(counter(before, action)).to.be.deep.equal(after)
   })
 
   it('Should increment another specific counter', () => {
     const before = [10, 20, 30,]
     const after = [10, 21, 30]
-    const action = { type: 'INCREMENT', index: 1 }
+    const action = { type: INCREMENT, index: 1 }
     expect(counter(before, action)).to.be.deep.equal(after)
   })
 
   it('Should decrement a specific counter', () => {
     const before = [1, 2, 3]
     const after = [1,1, 3]
-    const action = { type: 'DECREMENT', index: 1 }
+    const action = { type: DECREMENT, index: 1 }
     expect(counter(before, action)).to.be.deep.equal(after)
   })
 
   it('Should decrement another specific counter', () => {
     const before = [10, 20, 30,]
     const after = [10, 19, 30]
-    const action = { type: 'DECREMENT', index: 1 }
+    const action = { type: DECREMENT, index: 1 }
     expect(counter(before, action)).to.be.deep.equal(after)
   })
 })
