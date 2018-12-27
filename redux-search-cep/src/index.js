218aabf086cfa28a0a13edc3075282b6f4d67ab8
Lucas Mendes <lucaslsbh1@gmail.com>
2018-12-26 22:21:20 -0200

    [redux-search-cep] - Implementação do redux na aplicação

--- redux-search-cep/src/index.js
+++ redux-search-cep/src/index.js
@@ -2,6 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SearchCep from './containers/SearchCep';
 
+// Redux
+import rootReducer from './redux/reducers'
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunkMiddleware from 'redux-thunk'
+import { Provider } from 'react-redux'
+
+// Lib
 import 'milligram'
 
-ReactDOM.render(<SearchCep />, document.getElementById('root'));
+const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(
+      thunkMiddleware,
+    ),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  )
+)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <SearchCep />
+  </Provider>, document.getElementById('root'))
