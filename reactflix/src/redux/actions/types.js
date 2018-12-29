9dcb1dda0cc1d8ef2c55763872bf85d2e1e0e9b9
Lucas Mendes <lucaslsbh1@gmail.com>
2018-12-28 23:20:35 -0200

    [reactflix] - Criação do reducer de UI suportando o estado de formulário de cadastro aberto e fechado

--- reactflix/src/redux/actions/types.js
+++ reactflix/src/redux/actions/types.js
@@ -1,1 +1,5 @@
 export const ADD_VIDEO = 'ADD_VIDEO'
+
+// UI
+export const OPEN_REGISTER = 'OPEN_REGISTER'
+export const CLOSE_REGISTER = 'CLOSE_REGISTER'
---
+++ reactflix/src/redux/reducers/ui/index.js
@@ -0,0 +1,26 @@
+import { OPEN_REGISTER, CLOSE_REGISTER } from '../../actions/types'
+
+const initialState = {
+  isRegisterOpened: false
+}
+
+const ui = (state = initialState, action) => {
+  switch (action.type) {
+    case OPEN_REGISTER:
+      return {
+        ...state,
+        isRegisterOpened: true
+      }
+
+    case CLOSE_REGISTER:
+      return {
+        ...state,
+        isRegisterOpened: false
+      }
+    
+    default:
+      return state
+  }
+}
+
+export default ui
