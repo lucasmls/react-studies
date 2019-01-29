56b3eb363916aed37b849cba428ff40cfb01f54c
Lucas Mendes <lucaslsbh1@gmail.com>
2019-01-28 22:32:42 -0200

    [level-2-react] - Rotacionando o card ao realizar o "drag"

--- level-2-react/src/components/Drag/index.js
+++ level-2-react/src/components/Drag/index.js
@@ -1,7 +1,22 @@
 import React, { Component } from "react";
+import styled from "styled-components";
 import { Card } from "../../Elements/Cards";
 import { Gesture } from "react-with-gesture";
 import { Spring, animated, interpolate } from "react-spring";
+
+const DragCard = styled(Card)`
+  height: 300px;
+  position: absolute;
+  width: 320px;
+`;
+
+const CardContainer = styled.div`
+  position: relative;
+  background: #ccc;
+  height: 300px;
+  width: 320px;
+  margin: 0 auto;
+`;
 
 class Drag extends Component {
   render() {
@@ -16,14 +31,26 @@ class Drag extends Component {
             immediate={name => down && name === "x"}
           >
             {({ x }) => (
-              <Card
-                as={animated.div}
-                style={{
-                  transform: x.interpolate(x => `translateX(${x}px)`)
-                }}
-              >
-                <h1>Drag me {down}</h1>
-              </Card>
+              <CardContainer>
+                <DragCard
+                  as={animated.div}
+                  style={{
+                    transform: interpolate(
+                      [
+                        x,
+                        x.interpolate({
+                          range: [-300, 300],
+                          output: [-45, 45],
+                          extrapolate: "clamp"
+                        })
+                      ],
+                      (x, rotate) => `translateX(${x}px) rotate(${rotate}deg)`
+                    )
+                  }}
+                >
+                  <h1>Drag me {down}</h1>
+                </DragCard>
+              </CardContainer>
             )}
           </Spring>
         )}
