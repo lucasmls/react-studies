6a1c285004d916ee8693c46362e9ab4979e8ecd1
Lucas Mendes <lucaslsbh1@gmail.com>
2019-03-19 22:14:42 -0300

    [github-compare] - "Mockando" dados dos repositorios e renderizando baseado nas props

--- go-react/github-compare/src/components/CompareList/index.js
+++ go-react/github-compare/src/components/CompareList/index.js
@@ -1,60 +1,39 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 import { ListContainer, Repository } from './style'
 
-const CompareList = () => (
+const CompareList = ({ repositories }) => (
   <ListContainer>
-    <Repository>
-      <header>
-        <img
-          src='https://avatars3.githubusercontent.com/u/69631?v=4'
-          alt='facebook'
-        />
-        <strong>react</strong>
-        <small>facebook</small>
-      </header>
+    {repositories.map(repository => (
+      <Repository>
+        <header>
+          <img src={repository.owner.avatar_url} alt={repository.owner.login} />
+          <strong>{repository.name}</strong>
+          <small>{repository.owner.login}</small>
+        </header>
 
-      <ul>
-        <li>
-          123,122 <small>stars</small>
-        </li>
-        <li>
-          123,122 <small>forks</small>
-        </li>
-        <li>
-          123,122 <small>issues</small>
-        </li>
-        <li>
-          3 days ago <small>last commit</small>
-        </li>
-      </ul>
-    </Repository>
-    <Repository>
-      <header>
-        <img
-          src='https://avatars3.githubusercontent.com/u/69631?v=4'
-          alt='facebook'
-        />
-        <strong>react</strong>
-        <small>facebook</small>
-      </header>
-
-      <ul>
-        <li>
-          123,122 <small>stars</small>
-        </li>
-        <li>
-          123,122 <small>forks</small>
-        </li>
-        <li>
-          123,122 <small>issues</small>
-        </li>
-        <li>
-          3 days ago <small>last commit</small>
-        </li>
-      </ul>
-    </Repository>
+        <ul>
+          <li>
+            {repository.stargazers_count} <small>stars</small>
+          </li>
+          <li>
+            {repository.forks_count} <small>forks</small>
+          </li>
+          <li>
+            {repository.open_issues_count} <small>issues</small>
+          </li>
+          <li>
+            {repository.pushed_at} <small>last commit</small>
+          </li>
+        </ul>
+      </Repository>
+    ))}
   </ListContainer>
 )
+
+CompareList.propTypes = {
+  repositories: PropTypes.array.isRequired
+}
 
 export default CompareList
