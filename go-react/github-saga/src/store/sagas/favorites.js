fd950a630797d0d0cac994904bfd9887816a3cf5
Lucas Mendes <lucaslsbh1@gmail.com>
2019-03-23 21:07:22 -0300

    [github-saga] - Aplicando o duck pattern na organização dos arquivos do redux

--- go-react/github-saga/src/store/sagas/favorites.js
+++ go-react/github-saga/src/store/sagas/favorites.js
@@ -1,11 +1,12 @@
 import { call, put } from 'redux-saga/effects'
 import api from '../../services/api'
 
-import { addFavoriteSuccess, addFavoriteFailure } from '../actions/favorites'
+import { Creators as FavoriteActions } from '../ducks/favorites'
 
 export function * addFavorite (action) {
   try {
     const { data } = yield call(api.get, `/repos/${action.payload.repository}`)
+
     const repositoryData = {
       id: data.id,
       name: data.full_name,
@@ -13,8 +14,10 @@ export function * addFavorite (action) {
       url: data.html_url
     }
 
-    yield put(addFavoriteSuccess(repositoryData))
+    yield put(FavoriteActions.addFavoriteSuccess(repositoryData))
   } catch (error) {
-    yield put(addFavoriteFailure('Erro ao adicionar repositório'))
+    yield put(
+      FavoriteActions.addFavoriteFailure('Erro ao adicionar repositório')
+    )
   }
 }
