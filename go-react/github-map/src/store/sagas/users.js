ed41962dc91a5f530cb8c2e031a537254ea25d57
Lucas Mendes <lucaslsbh1@gmail.com>
2019-03-25 19:43:42 -0300

    [github-map] - Não permitindo adicionar usuários duplicados

--- go-react/github-map/src/store/sagas/users.js
+++ go-react/github-map/src/store/sagas/users.js
@@ -1,4 +1,4 @@
-import { call, put } from 'redux-saga/effects'
+import { call, put, select } from 'redux-saga/effects'
 import { toast } from 'react-toastify'
 
 import api from '../../services/api'
@@ -7,22 +7,38 @@ import { Creators as ModalCreators } from '../ducks/modal'
 
 export function * fetchUser (action) {
   try {
-    const { data } = yield call(api.get, `/users/${action.payload.username}`)
+    const isDuplicated = yield select(state =>
+      state.users.data.find(user => user.login === action.payload.username)
+    )
 
-    const user = {
-      name: data.name,
-      avatar_url: data.avatar_url,
-      login: data.login,
-      url: data.html_url,
-      ...action.payload.coordinates
+    if (isDuplicated) {
+      yield put(
+        UsersCreators.fetchUserFailure({
+          error: 'Este usuário já foi adicionado'
+        })
+      )
+
+      toast.warn('Este usuário já foi adicionado', {
+        position: toast.POSITION.TOP_RIGHT
+      })
+    } else {
+      const { data } = yield call(api.get, `/users/${action.payload.username}`)
+
+      const user = {
+        name: data.name,
+        avatar_url: data.avatar_url,
+        login: data.login,
+        url: data.html_url,
+        ...action.payload.coordinates
+      }
+
+      yield put(UsersCreators.fetchUserSuccess(user))
+      yield put(ModalCreators.closeUserModal())
+
+      toast.success('Usuário Adicionado com sucesso!', {
+        position: toast.POSITION.TOP_RIGHT
+      })
     }
-
-    yield put(UsersCreators.fetchUserSuccess(user))
-    yield put(ModalCreators.closeUserModal())
-
-    toast.success('Usuário Adicionado com sucesso!', {
-      position: toast.POSITION.TOP_RIGHT
-    })
   } catch (error) {
     yield put(
       UsersCreators.fetchUserFailure({ error: 'Usuário não encontrado' })
