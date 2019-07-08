136013c1df2b12dffd5a8f237e36924a89f54baf
Lucas Mendes <lucaslsbh1@gmail.com>
2019-07-07 23:18:43 -0300

    [context-hooks] - Salvando os livros adicionados no localStorage

--- context-hooks/src/contexts/BooksContext.js
+++ context-hooks/src/contexts/BooksContext.js
@@ -1,10 +1,18 @@
-import React, { useReducer, createContext } from "react";
+import React, { useReducer, useEffect, createContext } from "react";
 import { booksReducer } from "../reducers/books";
 
 export const BooksContext = createContext();
 
 export default function BooksContextProvider({ children }) {
-  const [books, dispatch] = useReducer(booksReducer, []);
+  const [books, dispatch] = useReducer(
+    booksReducer,
+    [],
+    () => JSON.parse(localStorage.getItem("@books")) || []
+  );
+
+  useEffect(() => {
+    localStorage.setItem("@books", JSON.stringify(books));
+  }, [books]);
 
   return (
     <BooksContext.Provider value={{ books, dispatch }}>
@@ -12,12 +20,3 @@ export default function BooksContextProvider({ children }) {
     </BooksContext.Provider>
   );
 }
-
-/* 
-  [
-    { id: 1, title: "the way of kings", author: "Brandon Sanderson" },
-    { id: 2, title: "the name of the wind", author: "Patrick Rothfuss" },
-    { id: 3, title: "the final empire", author: "Brandon Sanderson" },
-    { id: 4, title: "the fellowship of the ring", author: "J. R. R. Tolkien" }
-  ]
-*/
