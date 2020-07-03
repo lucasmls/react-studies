d26929c21ecd41a0a00c859ad44d529200728c68
Lucas Mendes <lucaslsbh1@gmail.com>
2020-07-02 22:14:37 -0300

    [next-boilerplate] - convert home page to .tsx

--- next-boilerplate/src/pages/index.tsx
+++ next-boilerplate/src/pages/index.tsx
@@ -1,10 +1,14 @@
 import Head from 'next/head'
 
-export default function Home() {
+type Props = {
+  title: string
+}
+
+export default function Home({ title = "React Avançado!" }: Props) {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>{title}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
